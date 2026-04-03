9c3c516c958965912a906941a02e01f314bb948d
RodrigoGarciaNunez <2203025158@cua.uam.mx>
2026-04-02 21:29:46 -0600

    added a tree direcotry page. Still trying to work it out

--- controller/main.py
+++ controller/main.py
@@ -2,8 +2,11 @@
 from flask import Flask, request, render_template, redirect
 from markupsafe import escape  # esto sirve para evitar inyecciones
 from werkzeug.middleware.proxy_fix import ProxyFix
+import os
+import requests
+import sys
 
-
+BASE_DIR = "files_container"
 
 #hola
 app = Flask(__name__, template_folder="../view")
@@ -19,11 +22,33 @@ def welcome(name = None):
 def sign_in():
     pass
 
-@app.route("/directory")
-def directory():
-    return redirect("/files_container/python/python_cs.pdf")
+@app.route("/biblioteca")
+def biblioteca():
+    structure = {}  #diccionario de la biblioteca
+
+    # for folder in os.listdir(BASE_DIR):
+    #     folder_path = os.path.join(BASE_DIR, folder)
+
+    #     if os.path.isdir(folder_path):
+    #         files = [f for f in os.listdir(folder_path) 
+    #                  if f.endswith(".pdf") and not f.startswith(".")]
+            
+    #         structure[folder] = files
+
+    # print("holaaa")
+    directorio = requests.get("http://files_container/")
+    # print(directorio)
+    # sys.stdout.flush()
+
+    print(directorio.status_code)
+    print(directorio.headers)
+    print(directorio.text)
+
+    sys.stdout.flush()
+
+    return render_template("biblioteca.html", structure=structure)
+
 
 
 if __name__ == '__main__':
-    print("hola")
     app.run(host='0.0.0.0', port=8080, debug=True)   
